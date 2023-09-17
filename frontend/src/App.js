92fd5581a519e3c4f7b0ba01b4229cb3d6e80c59
niklauswetter <niklauswetter@arizona.edu>
2023-09-16 20:32:45 -0700

    Initial Header component

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+        <Header title="Images Gallery"/>
+    </div>
+  );
+}
+
+export default App;
